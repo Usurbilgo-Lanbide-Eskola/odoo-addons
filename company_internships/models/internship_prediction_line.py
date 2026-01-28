29ebd44cef143c6a5f705e5ead5fc6844192e1ae
marregi <irmarregi@lhzubieta.eus>
2026-01-28 09:06:44 +0100

    [FIX]

--- company_internships/models/internship_prediction_line.py
+++ company_internships/models/internship_prediction_line.py
@@ -15,8 +15,8 @@ class InternshipPredictionLine(models.Model):
                                  related="school_year_historical_id.student_id",
                                  store=True)
     age = fields.Integer(string="Age", related="student_id.age")
-    driving_licence = fields.Boolean(
-        string="Driving Licence", related="student_id.driving_licence")
+    driving_license = fields.Boolean(
+        string="Driving License", related="student_id.driving_license")
     car_owned = fields.Boolean(
         string="Car Owned", related="student_id.car_owned")
     city = fields.Char(string="City", related="student_id.city")
